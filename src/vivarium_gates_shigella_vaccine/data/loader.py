60fc3fe74c3ea6cafcced34c54cf478e1ae3d7cc
James Collins <collijk@uw.edu>
2020-01-21 00:41:34 -0800

    Add in age bins and demographic dimensions

--- src/vivarium_gates_shigella_vaccine/data/loader.py
+++ src/vivarium_gates_shigella_vaccine/data/loader.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 from vivarium.framework.artifact import EntityKey
-from vivarium_inputs import utilities
+from vivarium_inputs import utilities, interface, utility_data, globals as vi_globals
 
 from vivarium_gates_shigella_vaccine import paths
 from vivarium_gates_shigella_vaccine import globals as project_globals
@@ -37,30 +37,32 @@ def load_population_structure(key: EntityKey, location: str):
     return utilities.sort_hierarchical_data(data)
 
 
-def load_age_bins(key: EntityKey, location_id: int):
-    return extract.get_age_bins()
+def load_age_bins(key: EntityKey, location: str):
+    return interface.get_age_bins()
 
 
-def load_demographic_dimensions(key: EntityKey, location_id: int):
-    age_bins = extract.get_age_bins().loc[:, ['age_group_start', 'age_group_end']]
-
-    sex_data = []
-    for sex in ['Male', 'Female']:
-        df = age_bins.copy()
-        df['sex'] = sex
-        sex_data.append(df)
-
-    sex_data = pd.concat([sex_data], ignore_index=True)
-
+def load_demographic_dimensions(key: EntityKey, location: str):
+    location_id = extract.get_location_id(location)
+    ages = utility_data.get_age_group_ids()
     years = range(project_globals.MIN_YEAR, project_globals.MAX_YEAR + 1)
-    year_data = []
-    for year_start in years:
-        df = sex_data.copy()
-        df['year_start'] = year_start
-        df['year_end'] = year_start + 1
-        year_data.append(df)
+    sexes = [vi_globals.SEXES['Male'], vi_globals.SEXES['Female']]
+    location = [location_id]
+    values = [location, sexes, ages, years]
+    names = ['location_id', 'sex_id', 'age_group_id', 'year_id']
 
-    return pd.concat(year_data, ignore_index=True)
+    data = (pd.MultiIndex
+            .from_product(values, names=names)
+            .to_frame(index=False))
+
+    data = standardize.normalize(data)
+    data = utilities.reshape(data)
+    data = utilities.scrub_gbd_conventions(data, location)
+    data = utilities.split_interval(data, interval_column='age', split_column_prefix='age')
+    data = utilities.split_interval(data, interval_column='year', split_column_prefix='year')
+    return utilities.sort_hierarchical_data(data)
+
+
+
 
 
 
