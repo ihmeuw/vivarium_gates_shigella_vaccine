1f6d3cab628f3eccfab77127e6ea92fa1ee0ebbf
James Collins <collijk@uw.edu>
2020-01-21 00:31:04 -0800

    Start the restructure to use modern tools for data cleaning

--- src/vivarium_gates_shigella_vaccine/data/loader.py
+++ src/vivarium_gates_shigella_vaccine/data/loader.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 from vivarium.framework.artifact import EntityKey
+from vivarium_inputs import utilities
 
 from vivarium_gates_shigella_vaccine import paths
 from vivarium_gates_shigella_vaccine import globals as project_globals
@@ -16,16 +17,24 @@ def get_data(key: EntityKey, location: str):
         EntityKey('population.age_bins'): load_age_bins,
         EntityKey('population.demographic_dimensions'): load_demographic_dimensions,
     }
+
+    return mapping[key](key, location)
+
+
+def load_population_structure(key: EntityKey, location: str):
     location_id = extract.get_location_id(location)
-    return mapping[key](key, location_id)
-
-
-def load_population_structure(key: EntityKey, location_id: int):
     path = paths.forecast_data_path(key)
     data = extract.load_forecast_from_xarray(path, location_id)
-    value_column = 'population'
-    data = standardize.normalize_forecasting(data, value_column, sexes=['Male', 'Female', 'Both'])
-    return data[BASE_COLUMNS + [value_column]]
+    data = data[data.scenario == project_globals.FORECASTING_SCENARIO].drop(columns='scenario')
+    data = data.set_index(['location_id', 'age_group_id', 'sex_id', 'year_id', 'draw']).unstack()
+    data.columns = pd.Index([f'draw_{i}' for i in range(1000)])
+    data = data.reset_index()
+    data = standardize.normalize(data)
+    data = utilities.reshape(data)
+    data = utilities.scrub_gbd_conventions(data, location)
+    data = utilities.split_interval(data, interval_column='age', split_column_prefix='age')
+    data = utilities.split_interval(data, interval_column='year', split_column_prefix='year')
+    return utilities.sort_hierarchical_data(data)
 
 
 def load_age_bins(key: EntityKey, location_id: int):
--- src/vivarium_gates_shigella_vaccine/data/standardize.py
+++ src/vivarium_gates_shigella_vaccine/data/standardize.py
@@ -1,14 +1,48 @@
 """All generic functionality for standardizing raw data."""
 import math
+from numbers import Real
+from typing import List
 
 import pandas as pd
+from vivarium_inputs import globals as vi_globals
+from vivarium_inputs import utilities
 
 from vivarium_gates_shigella_vaccine import globals as project_globals
 
-from .extract import get_age_bins, get_age_group_id
-
 
 FERTILE_AGE_GROUP_IDS = list(range(7, 15 + 1))  # need for calc live births by sex
+
+
+###############################################################
+# Functions to normalize GBD data over a standard demography. #
+###############################################################
+
+def normalize(data: pd.DataFrame, fill_value: Real = None,
+              cols_to_fill: List[str] = vi_globals.DRAW_COLUMNS) -> pd.DataFrame:
+    data = utilities.normalize_sex(data, fill_value, cols_to_fill)
+    data = normalize_year(data)
+    data = utilities.normalize_age(data, fill_value, cols_to_fill)
+    return data
+
+
+def normalize_year(data: pd.DataFrame) -> pd.DataFrame:
+    years = range(project_globals.MIN_YEAR, project_globals.MAX_YEAR + 1)
+
+    if 'year_id' not in data:
+        # Data doesn't vary by year, so copy for each year.
+        df = []
+        for year in years:
+            fill_data = data.copy()
+            fill_data['year_id'] = year
+            df.append(fill_data)
+        data = pd.concat(df, ignore_index=True)
+    elif set(data.year_id) >= set(years):
+        # We have at least the required years, so filter down
+        data = data.loc[data.year_id.isin(years)]
+    else:  # We don't have a strategy.
+        raise ValueError('Years in data are incompatible with normalization strategies.')
+
+    return data
 
 
 def normalize_for_simulation(df):
