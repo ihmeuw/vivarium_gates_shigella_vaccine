da794c6cdddf6cd9de07aed76db3722151ecc1e3
James Collins <collijk@uw.edu>
2020-01-20 20:19:43 -0800

    Sketch out parallel build

--- src/vivarium_gates_shigella_vaccine/tools/make_artifacts.py
+++ src/vivarium_gates_shigella_vaccine/tools/make_artifacts.py
@@ -7,6 +7,7 @@
 
 """
 from pathlib import Path
+import shutil
 
 import click
 from loguru import logger
@@ -53,7 +54,47 @@ def build_artifacts(location: str, output_dir: str, append: bool):
 
 
 def build_all_artifacts(output_dir):
-    pass
+    from vivarium_cluster_tools.psimulate.utilities import get_drmaa
+    drmaa = get_drmaa()
+
+    jobs = {}
+    with drmaa.Session() as session:
+        for location in project_globals.LOCATIONS:
+            path = output_dir / f'{sanitize_location(location)}.hdf'
+
+            command = [
+                f'from {project_globals.PROJECT_NAME}.tools.make_artifacts import build_single_location_artifact',
+                f'build_single_location_artifact("{str(path)}", "{location}")'
+            ]
+
+            job_template = session.createJobTemplate()
+            job_template.remoteCommand = shutil.which('python')
+            job_template.args = ['-c', '; '.join(command)]
+            job_template.nativeSpecification = (f'-V -P {project_globals.CLUSTER_PROJECT} -q all.q'
+                                                f'-l fmem=3G -l fthread=1 -l h_rt=3:00:00 '
+                                                f'-N {sanitize_location(location)}_artifact')
+            jobs[location] = (session.runJob(job_template), drmaa.JobState.UNDETERMINED)
+            logger.info(f'Submitted job {jobs[location]} to build artifact for {location}.')
+            session.deleteJobTemplate(job_template)
+
+        decodestatus = {drmaa.JobState.UNDETERMINED: 'process status cannot be determined',
+                        drmaa.JobState.QUEUED_ACTIVE: 'job is queued and active',
+                        drmaa.JobState.SYSTEM_ON_HOLD: 'job is queued and in system hold',
+                        drmaa.JobState.USER_ON_HOLD: 'job is queued and in user hold',
+                        drmaa.JobState.USER_SYSTEM_ON_HOLD: 'job is queued and in user and system hold',
+                        drmaa.JobState.RUNNING: 'job is running',
+                        drmaa.JobState.SYSTEM_SUSPENDED: 'job is system suspended',
+                        drmaa.JobState.USER_SUSPENDED: 'job is user suspended',
+                        drmaa.JobState.DONE: 'job finished normally',
+                        drmaa.JobState.FAILED: 'job finished, but failed'}
+
+        logger.info('Entering monitoring loop.')
+        while any([False if job[1] in [drmaa.JobState.DONE, drmaa.JobState.FAILED] else True for job in jobs.values()]):
+            for location, (job_id, status) in jobs.items():
+                jobs[location] = (job_id, decodestatus[session.job_status(job_id)])
+                logger.info(f'{location}: {jobs[location]}')
+
+    logger.info('Done')
 
 
 def build_single_location_artifact(path, location):
