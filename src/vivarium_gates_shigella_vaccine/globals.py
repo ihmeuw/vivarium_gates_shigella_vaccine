2f7f6aa99c865c3ff285d812ea6958a8d4010629
James Collins <collijk@uw.edu>
2020-01-21 20:05:04 -0800

    remission and disability weight

--- src/vivarium_gates_shigella_vaccine/globals.py
+++ src/vivarium_gates_shigella_vaccine/globals.py
@@ -1,6 +1,3 @@
-from types import SimpleNamespace
-
-
 PROJECT_NAME = 'vivarium_gates_shigella_vaccine'
 CLUSTER_PROJECT = 'proj_cost_effect_diarrhea'
 
@@ -10,19 +7,6 @@ MIN_YEAR = 2017
 MAX_YEAR = 2040
 NUM_DRAWS = 1000
 
-DIARRHEAL_DISEASES = SimpleNamespace(
-    dismod_id=1181,
-    restrictions=SimpleNamespace(
-        male_only=False,
-        female_only=False,
-        yll_only=False,
-        yld_only=False,
-        yll_age_group_id_start=2,
-        yll_age_group_id_end=235,
-        yld_age_group_id_start=2,
-        yld_age_group_id_end=235,
-    )
-)
 
 LOCATIONS = [
     'Bangladesh',
